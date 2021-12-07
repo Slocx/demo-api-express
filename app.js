7fc43b32ec3d34e590fac96e32354dad1b897a8a
Slocx <33150302+Slocx@users.noreply.github.com>
2021-12-07 15:32:14 +0100

    commit add routes

--- app.js
+++ app.js
@@ -1,49 +1,17 @@
 // import d'express
 const express = require('express');
 
+// import des routes
+const userRoute = require('./routes/users.route');
+
 // création de l'app
 const app = express();
 
-// CRUD
-// Create: app.post()               HTTP POST
-// Read: app.get()                  HTTP GET
-// Update: app.put() ou app.patch() HTTP PUT OU PATCH
-// Delete: app.delete()             HTTP DELETE
+// Middlewares
+app.use(express.json([])); // Middleware pour passer le JSON
 
-/*
-*   Récupère les utilisateurs
-*/
-app.get('/users', (req, res) => {
-    console.log(`${req.method} sur ${req.path}`);
-    res.end('OK');
-});
-
-/*
-*   Récupère un utilisateur avec son id donné en param de route
-*/
-app.get('/users/:id', (req, res) => {
-
-});
-/*
-*   Créer un utilisateur
-*/
-app.post('/users', (req, res) => {
-
-});
-/*
-*   Mets à jour un utilisateur
-*/
-app.put('/users/:id', (req, res) => {
-
-});
-/*
-*   Supprime un utilisateur
-*/
-app.delete('/users/:id', (req, res) => {
-
-});
-
-
+// utilisation des routes
+app.use(userRoute);
 
 // écoute sur le port donné en paramètre
 app.listen(3000);
